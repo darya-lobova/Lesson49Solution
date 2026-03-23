66c725a2066d15b0ccda679d638f518971114296
st <st@A510PC3>
2026-03-23 17:48:53 +0300

    update 2

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -20,16 +20,26 @@ void sort_selected(int array[], int length) {
 	}
 }
 
+//O(N^2)
+// Best case - O(N)
 void sort_inserted(int array[], int length) {
+
+	int count_inner = 0;
+	int count_outer = 0;
+
 	for (int i = 1; i < length; i++)
 	{
+		count_outer++;
 		int key = array[i];
 		int j = i - 1;
 
 		while (j >= 0 && array[j] > key) {
 			array[j + 1] = array[j];
 			j--;
+			count_inner++;
 		}
 		array[j + 1] = key;
 	}
+	cout << "Inner count: " << count_inner << endl;
+	cout << "Outer count: " << count_outer << endl;
 }
--- Task01/main.cpp
+++ Task01/main.cpp
@@ -1,15 +1,15 @@
 #include "util.h"
 
 int main() {
-	srand(time(NULL));
+	/*srand(time(NULL));*/
 
-	int array[DEFAULT_SIZE];
-	int size;
+	int array[]{1,3,4,5,6,7,9,10,11,12,2,13,14,15,8,16,17,18,19,20 };
+	int size = 20;
 
-	cout << "Input size of array: ";
+	/*cout << "Input size of array: ";
 	cin >> size;
 
-	init_random(array, size, 0, 100);
+	init_random(array, size, 0, 100);*/
 
 	/*long long start = time(NULL);*/
 
