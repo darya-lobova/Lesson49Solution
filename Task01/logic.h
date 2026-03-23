5e43b1bb15eafdf67d1ca2a780ecd90654831f01
st <st@A510PC3>
2026-03-23 16:54:25 +0300

    update 1

--- Task01/logic.h
+++ Task01/logic.h
@@ -1,1 +1,10 @@
-#pragma once
+#include <iostream>
+#include <string>
+
+#define DEFAULT_SIZE 100
+
+using namespace std;
+
+void sort_bubble(int array[], int length);
+void sort_selected(int array[], int length);
+void sort_inserted(int array[], int length);
